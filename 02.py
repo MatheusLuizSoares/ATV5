77869e4f33c6b31b54dcb21532810ebb97a29644
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2023-12-16 16:38:41 -0300

    02

--- 02.py
+++ 02.py
@@ -1,13 +1,27 @@
-import requests, datetime, os
+import requests
+import datetime
+import os
+import sys
+import json
+
 strURL = 'https://api.cartolafc.globo.com/atletas/mercado'
+strDiretorio = os.path.abspath(__file__)
+strDiretorio = os.path.dirname(strDiretorio)
 Ano_atual = datetime.datetime.now().year
-
 while True:
-    ano=(int("Escolha um ano até 2021>="))
-    if ano>Ano_atual:
-     break
-if ano==Ano_atual:
-    dic_cartolA=requests.get(strURL, verify=False).json()
-
-
-esquemas = ('343','352','433','442','451','532','541')
+    ano = int(input("Escolha um ano ou digite 0 para sair:"))
+    
+    if ano == 0:
+        print("Você está saindo do programa")
+        sys.exit()
+    
+    if ano > Ano_atual:
+        break
+    elif ano == Ano_atual:
+        dic_cartola = requests.get(strURL, verify=True).json()
+        break
+    else:
+     strNomeArq = f"{strDiretorio}/cartola_fc_{ano}.json"
+    with open(strNomeArq, "r", encoding='UTF-8') as dictOpen:
+        dic_cartola = json.load(dictOpen)
+  
