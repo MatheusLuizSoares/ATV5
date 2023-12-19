7f427c970fd6286bd68bde3ea530ec67f53be61d
Matheus Luiz Soares <matheus.luiz1@academico.ifrn.edu.br>
2023-12-19 20:22:26 -0300

    começando exe 03

---
+++ 01.py
@@ -0,0 +1,8 @@
+import requests,datetime,os, sys, json
+
+servidores_campi.csv 
+ strNomeArq = f"{strDiretorio}/cartola_fc_{ano}.json"
+        with open(strNomeArq, "r", encoding='UTF-8') as dictOpen:
+            dic_cartola = json.load(dictOpen)
+            dictOpen.close()
+            break
